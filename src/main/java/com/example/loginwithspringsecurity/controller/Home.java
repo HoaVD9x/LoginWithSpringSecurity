871f00ccfdfea9ca814b4b9f18bd6fe0dfa31119
DinhHoa9X <78968184+vuongdinhhoa96@users.noreply.github.com>
2022-12-11 15:22:23 +0900

    success home page

---
+++ src/main/java/com/example/loginwithspringsecurity/controller/Home.java
@@ -0,0 +1,26 @@
+package com.example.loginwithspringsecurity.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.security.authentication.BadCredentialsException;
+//import org.springframework.security.authentication.LockedException;
+//import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+//import org.springframework.security.core.Authentication;
+//import org.springframework.security.core.context.SecurityContextHolder;
+//import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+@Controller
+public class Home {
+    @GetMapping("/")
+    public String home(HttpServletRequest request, HttpSession session) {
+        return "index";
+    }
+
+}
