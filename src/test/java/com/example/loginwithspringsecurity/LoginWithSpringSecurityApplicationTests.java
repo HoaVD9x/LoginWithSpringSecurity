64e51dccf92184a0e476cedda9d864d9a7894061
DinhHoa9X <78968184+vuongdinhhoa96@users.noreply.github.com>
2022-12-11 13:59:14 +0900

    11/12/2022

---
+++ src/test/java/com/example/loginwithspringsecurity/LoginWithSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.loginwithspringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginWithSpringSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
